7461be9835088505d7d1b1b68935dd22663b8938
RadioMediocre <qiwen108@gmail.com>
2024-05-28 20:58:55 -0700

    Updated part 2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,20 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  
+  
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", async () => {
+      try {
+        const registration = await navigator.serviceWorker.register("./sw.js");
+        if (registration.active) {
+          console.log("Service worker register sucessful");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
 }
 
 /**
